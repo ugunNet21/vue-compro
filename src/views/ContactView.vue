b7089973e542f89675bec0f1a288c50c8820239e
Gugun Gunawan <gugun.net21@gmail.com>
2024-02-26 11:30:17 +0700

    -

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>Ini halaman  About</h1>
+    <h1>Ini halaman  Contact</h1>
   </div>
 </template>
 
--- src/views/ServiceView.vue
+++ src/views/ServiceView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>Ini halaman  About</h1>
+    <h1>Ini halaman  Service</h1>
   </div>
 </template>
 
